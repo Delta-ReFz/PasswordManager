ef4801966fdf35c386e3c9bef038c8b6d14ba3a2
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-12-28 01:28:35 -0500

    Completed

--- src/password_manager.py
+++ src/password_manager.py
@@ -1,11 +1,32 @@
-master_pwd = input("What is the master password? ")
+from cryptography.fernet import Fernet
+
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+
+
+def load_key():
+   file = open("key.key", "rb") #rb === reading bytes
+   key = file.read() # read the file
+   file.close()
+   return key
+
+
+key = load_key()  #we are converting our master password in bytes
+fer = Fernet(key)
+
+
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip() #rstrip will strip the carriage return
             user , passw = data.split("|")
-            print("User:", user,"\nPassword:", passw)
+            print("User:", user,"\nPassword:", fer.decrypt(passw.encode()).decode())
 
 
 
@@ -15,11 +36,7 @@ def add():
 
     #with open == closes the file when we finished using it, 'a' mode means that you can add something in the existing file, or if the file dosent exist it will create a new file named 'password.txt'
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")
-
-
-
-
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
